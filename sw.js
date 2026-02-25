114a5a7a4121877966e6db5004095ccb470f78fb
GWiz2077 <139511438+GWiz2077@users.noreply.github.com>
2026-02-25 12:19:37 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = "catgame-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js",
+
+  // add your actual asset paths here:
+  // "./cat.png",
+  // "./jump.jpg",
+  // "./bg.png",
+  // "./sound/jump.mp3",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k))))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
